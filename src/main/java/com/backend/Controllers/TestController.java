faf6278c3545284df2e7f349349d96b35ab2d253
skill-in-projects <admin@skill-in.com>
2026-01-14 18:46:27 +0200

    Update TestController.java

--- src/main/java/com/backend/Controllers/TestController.java
+++ src/main/java/com/backend/Controllers/TestController.java
@@ -15,7 +15,7 @@ import java.sql.Connection;
 import java.sql.Statement;
 import java.util.List;
 import java.util.Map;
-dddss
+dddssnm
 @RestController
 @RequestMapping("/api/test")
 @CrossOrigin(origins = "*")
